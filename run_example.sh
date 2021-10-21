33f22a08b9a0a6a28280fe78f928c4c8b31deb90
Justin Lübbers <justin.luebbers@studium.uni-hamburg.de>
2021-10-21 04:57:22 +0200

    example files

--- run_example.sh
+++ run_example.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-python lea.py -A example_files/cyp.fasta -B example_files/cpr.fasta -R example_files/fusionproteins.fasta
+python lea.py -A example_input_files/cyp.fasta -B example_input_files/cpr.fasta -R example_input_files/fusionproteins.fasta -o example_output_files
